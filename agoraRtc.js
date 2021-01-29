1083b34f428cde7fb1f7f468c3a2126aafb233d5
Olivia Cao <oliviacao@dynamic-oit-vapornet100-10-9-145-182.princeton.edu>
2021-01-29 13:48:15 -0500

    added login screen:

--- agoraRtc.js
+++ agoraRtc.js
@@ -2,6 +2,30 @@ let handlefail = function(err){
     console.log(err)
 }
 
+let appId = "6375fbbc160b4b96b715e3000e89682f";
+let globalStream;
+let isAudioMuted = false;
+let isVideoMuted = false;
+
+let client = AgoraRTC.createClient({
+    mode: "live",
+    codec: "h264"
+})
+
+client.init(appId,() => console.log("AgoraRTC Client Connected"),handlefail
+)
+
+function removeMyVideoStream(streamId) {
+    globalStream.stop();
+}
+
+function removeVideoStream(evt) {
+    let stream = evt.stream;
+    stream.stop;
+    let remDiv = document.getElementById(stream.getId());
+    remDiv.parentNode.removeChild(remDiv);
+}
+
 function addName(streamId) {
     let nameList = document.getElementById("participantList");
     let participantName = document.createElement("span");
@@ -23,10 +47,10 @@ function addVideoStream(streamId){
 
     streamDiv.id = streamId;
     streamDiv.style.transform = "rotateY(180deg)";
-    streamDiv.style.height = "150px";
+    streamDiv.style.height = "200px";
     streamDiv.style.width = "350px";
     streamDiv.style.textAlign = "left";
-    streamDiv.style.marginTop = "15px";
+    streamDiv.style.marginTop = "30px";
     streamDiv.style.justifySelf = "center";
     streamDiv.style.border = "2px solid #099dfd";
 
@@ -36,49 +60,84 @@ function addVideoStream(streamId){
     remoteContainer.appendChild(streamDiv);
 } 
 
-document.getElementById("join").onclick = function () {
-    let channelName = document.getElementById("channelName").value;
-    let Username = document.getElementById("username").value;
-    let appId = "6375fbbc160b4b96b715e3000e89682f";
-
-    let client = AgoraRTC.createClient({
-        mode: "live",
-        codec: "h264"
-    })
-
-    client.init(appId,() => console.log("AgoraRTC Client Connected"),handlefail
+document.getElementById("leave").onclick = function () {
+    client.leave(function() {
+        console.log("Left!")
+    },handlefail
     )
+    removeMyVideoStream();
+}
 
-    client.join(
-        null,
-        channelName,
-        Username,
-        () =>{
-            var localStream = AgoraRTC.createStream({
-                video: true,
-                audio: true,
-            })
+let channelName = localStorage["channelName"];
+let Username = localStorage["username"];
 
-            localStream.init(function(){
-                localStream.play("SelfStream")
-                addName(Username)
-                console.log(`App id: ${appId}\nChannel id: ${channelName}`)
-                client.publish(localStream)
-            })
-        }
-    )
+client.join(
+    null,
+    channelName,
+    Username,
+    () =>{
+        var localStream = AgoraRTC.createStream({
+            video: true,
+            audio: true,
+        })
 
-    client.on("stream-added", function (evt){
-        console.log("Added Stream");
-        client.subscribe(evt.stream,handlefail)
-    })
+        localStream.init(function(){
+            localStream.play("SelfStream")
+            addName(Username)
+            console.log(`App id: ${appId}\nChannel id: ${channelName}`)
+            client.publish(localStream)
+        })
 
-    client.on("stream-subscribed", function(evt){
-        console.log("Subscribed Stream");
-        let stream = evt.stream;
-        addVideoStream(stream.getId());  
-        addName(stream.getId());
-        stream.play(stream.getId());
-    })
+        globalStream = localStream
+    }
+)
 
+
+client.on("stream-added", function (evt){
+    console.log("Added Stream");
+    client.subscribe(evt.stream,handlefail)
+})
+
+client.on("stream-subscribed", function(evt){
+    console.log("Subscribed Stream");
+    let stream = evt.stream;
+    addVideoStream(stream.getId());  
+    addName(stream.getId());
+    stream.play(stream.getId());
+})
+
+client.on("peer-leave", function(evt) {
+    console.log("Peer has left")
+    removeVideoStream(evt)
+}) 
+
+
+
+document.getElementById("video-mute").onclick = function(evt){
+    if(!isVideoMuted){
+        globalStream.muteVideo()
+        isVideoMuted = true;
+        console.log("mute video")
+    } 
+    else {
+        globalStream.unmuteVideo();
+        isVideoMuted = false;
+        console.log("unmute video")
+
+    }
+}
+
+document.getElementById("audio-mute").onclick = function(){
+    if(!isAudioMuted){
+        globalStream.muteAudio()
+        isAudioMuted = true;
+        console.log("mute audio")
+
+    } 
+    else {
+        globalStream.unmuteAudio();
+        isAudioMuted = false;
+        console.log("unmute audio")
+
+    }
 }
---
+++ login.js
@@ -0,0 +1,6 @@
+localStorage["isMainPersonIn"] = false;
+document.getElementById("join").onclick = function () {
+    window.location.href = "video.html";
+    localStorage["username"] = document.getElementById("username").value;
+    localStorage["channelName"] = document.getElementById("channelName").value;
+}  
